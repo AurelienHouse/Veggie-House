fe8c7333ff25ec450083910a7f899f2e382312d2
AurelienHouse <aurelien.maison@gmail.com>
2024-04-01 06:36:21 +0200

    ✨ BH: Add "Router with HomePage"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
+import Router from './AppRouter'
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <>
+    <Router />
+    <h1 className="text-3xl font-bold underline bg bg-red-500">
       Burgers House !
     </h1>
+    </>
   )
 }
