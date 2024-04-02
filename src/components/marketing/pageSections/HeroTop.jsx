f6fac5f1699a9dce29f150b4dd9695450f434787
AurelienHouse <aurelien.maison@gmail.com>
2024-04-02 05:06:13 +0200

    ✨ HeroTop: Create Component "HeroTop"

--- src/components/marketing/pageSections/HeroTop.jsx
+++ src/components/marketing/pageSections/HeroTop.jsx
@@ -1,7 +1,8 @@
-
+import Illustration from '../../../medias/images/background/bg-food.jpg'
 export default function HeroTop() {
   return (
-    <div className="bg-pr">
+    <div className="bg-primary w-full">
+      <div className="bg-repeat w-full h-full" style={{ backgroundImage: `url(${Illustration})`}}></div>
         
     </div>
   )
