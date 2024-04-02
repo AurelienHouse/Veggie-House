fd6ebc7ace853a4e6b51bbdf4d9686270b705899
AurelienHouse <aurelien.maison@gmail.com>
2024-04-02 21:38:34 +0200

    💄 HeroTop: Create "Button" and setup Header

--- src/components/marketing/pageSections/Header.jsx
+++ src/components/marketing/pageSections/Header.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
 import Logo from '../../../medias/svg/Logo-burger-house.svg'
+import Icone from '../../../medias/svg/ico-bag-clickAndCollect.svg'
+import Button from '../../elements/Button'
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between'>
+    <div className='flex items-center justify-between py-10'>
+        <div className="w-full">
           <img src={Logo} alt='logo Burger House App' className='w-64'/>
+        </div>
+        <div className=" w-full text-secondary ">
+          
+          <div className="flex items-center justify-end">
+            <img src={Icone} alt="" className="w-5 h-5 mr-1"  />
+            <span>Commandez votre repas en ligne</span>
+          </div>
+          <div className="">
+            <Button />
+          </div>
+        </div>
+            
 
     </div>
   )
