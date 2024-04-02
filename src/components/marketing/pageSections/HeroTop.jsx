3875a0549c3eb966937a38f92e5073fb8c5cf3dc
AurelienHouse <aurelien.maison@gmail.com>
2024-04-02 22:53:16 +0200

    🎨 HeroTop: Setup Text and Img

--- src/components/marketing/pageSections/HeroTop.jsx
+++ src/components/marketing/pageSections/HeroTop.jsx
@@ -1,6 +1,7 @@
 import Illustration from '../../../medias/images/background/bg-food.jpg'
 import Container from '../../elements/Container'
 import Header from './Header'
+import Products from '../../../medias/images/heroTop/burger-hero-top.png'
 
 export default function HeroTop() {
   return (
@@ -9,6 +10,19 @@ export default function HeroTop() {
 
         <Container>
           <Header />
+          <div className=" w-full flex items-center">
+            <div className=" text-secondary uppercase w-2/3">
+              <h1 className='mb-7'>Le moment de goûter au bon goût des hamburgers.</h1>
+              <h2 className="font-secondary">
+                <span className='text-8xl block'>Burger</span>
+                <span className='text-6xl'>House</span> 
+                <span className="text-4xl ml-3">Click&Collect</span></h2>
+
+            </div>
+            <div className=" w-full">
+              <img src={Products} alt="image d'un menu Hamburger frites et soda" className="" />
+            </div>
+          </div>
         </Container>
 
       </div>
