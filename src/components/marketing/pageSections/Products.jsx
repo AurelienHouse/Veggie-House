31a60615a1cc4b166accc2106380c9d1d4a201a4
AurelienHouse <aurelien.maison@gmail.com>
2024-04-04 19:14:34 +0200

    ✨ Products: Create "headingTitle"

---
+++ src/components/marketing/pageSections/Products.jsx
@@ -0,0 +1,14 @@
+import Container from "../../elements/Container";
+import HeadingTitle from "../../elements/DisplayTitles/HeadingTitle";
+
+export default function Products() {
+  return (
+    <Container>
+        <HeadingTitle variant='h3'>
+            Toujours des délicieux Burgers
+
+        </HeadingTitle>
+    </Container>
+    
+  )
+}
