a23ad5ec89f3a17e9a19393a44be5571896e0dab
AurelienHouse <aurelien.maison@gmail.com>
2024-04-06 00:06:34 +0200

    🎨 BH: FormatCode only

--- src/components/marketing/pageSections/Events.jsx
+++ src/components/marketing/pageSections/Events.jsx
@@ -1,79 +1,82 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
-import Container from "../../elements/Container"
-import Heading from "../../elements/DisplayTitles/Heading"
-import AliceCarousel from 'react-alice-carousel';
-import '../../../css/carousel.css';
-import Image01 from "../../../medias/images/events/dorian-hurst-d-igN2ptyC8-unsplash.jpg"
-import Image02 from "../../../medias/images/products/Product-1.jpg"
-import Image03 from "../../../medias/images/products/Product-2.jpg"
+import Container from "../../elements/Container";
+import Heading from "../../elements/DisplayTitles/Heading";
+import AliceCarousel from "react-alice-carousel";
+import "../../../css/carousel.css";
+import Image01 from "../../../medias/images/events/dorian-hurst-d-igN2ptyC8-unsplash.jpg";
+import Image02 from "../../../medias/images/products/Product-1.jpg";
+import Image03 from "../../../medias/images/products/Product-2.jpg";
 
-
-const Slide = ({children, category, title, image}) => {
-    return(
-        <div className="grid grid-cols-2 p-6">
-            <div className="p-10 mb-10">
-                    <Heading variant="h4">
-                    {category}
-                    </Heading>
-                    <Heading theme="secondary" variant="h3" className="mb-5">
-                    {title}
-                    </Heading>
-                    <p className="tracking-wide text-lg leading-relaxed text-gray-800 font-light">
-                    {children} 
-                    </p>
-            </div>
-        <div className="relative w-full h-full">
-        <img src={image} alt="Image d'un evenement à venir" className="bg-primary absolute top-0 left-0 w-full h-full object-cover object-bottom z-0" />
-        </div>
-
+const Slide = ({ children, category, title, image }) => {
+  return (
+    <div className="grid grid-cols-2 p-6">
+      <div className="p-10 mb-10">
+        <Heading variant="h4">{category}</Heading>
+        <Heading theme="secondary" variant="h3" className="mb-5">
+          {title}
+        </Heading>
+        <p className="tracking-wide text-lg leading-relaxed text-gray-800 font-light">
+          {children}
+        </p>
+      </div>
+      <div className="relative w-full h-full">
+        <img
+          src={image}
+          alt="Image d'un evenement à venir"
+          className="bg-primary absolute top-0 left-0 w-full h-full object-cover object-bottom z-0"
+        />
+      </div>
     </div>
-    )
-}
+  );
+};
 export default function Events() {
-
-
-const items = [
-	<Slide
-    image={Image01}
-    category="evenement à venir"
-    title="vivez la demi-finale"
+  const items = [
+    <Slide
+      image={Image01}
+      category="evenement à venir"
+      title="vivez la demi-finale"
     >
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates temporibus sapiente enim molestiae, perspiciatis cum amet, eius consectetur voluptate dicta odit officia fugit atque consequatur a cupiditate labore repudiandae quae?
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+      temporibus sapiente enim molestiae, perspiciatis cum amet, eius
+      consectetur voluptate dicta odit officia fugit atque consequatur a
+      cupiditate labore repudiandae quae?
     </Slide>,
     <Slide
-    image={Image02}
-    category="Bientôt disponible"
-    title="Nouveau Burger Sauce Maison"
+      image={Image02}
+      category="Bientôt disponible"
+      title="Nouveau Burger Sauce Maison"
     >
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates temporibus sapiente enim molestiae, perspiciatis cum amet, eius consectetur voluptate dicta odit officia fugit atque consequatur a cupiditate labore repudiandae quae?
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+      temporibus sapiente enim molestiae, perspiciatis cum amet, eius
+      consectetur voluptate dicta odit officia fugit atque consequatur a
+      cupiditate labore repudiandae quae?
     </Slide>,
     <Slide
-    image={Image03}
-    category="Bientôt disponible"
-    title="Nouveau Double Burger"
+      image={Image03}
+      category="Bientôt disponible"
+      title="Nouveau Double Steak"
     >
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates temporibus sapiente enim molestiae, perspiciatis cum amet, eius consectetur voluptate dicta odit officia fugit atque consequatur a cupiditate labore repudiandae quae?
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+      temporibus sapiente enim molestiae, perspiciatis cum amet, eius
+      consectetur voluptate dicta odit officia fugit atque consequatur a
+      cupiditate labore repudiandae quae?
     </Slide>,
-    
-    
-    
-];
+  ];
 
   return (
-
-    
     <Container>
-        <div className="shadow-2xl mb-20">
-        <AliceCarousel mouseTracking items={items} 
-        autoPlay
-        disableButtonsControls
-        infinite
-        autoPlayInterval="4000"
-        animationType="fadeout"
+      <div className="shadow-2xl mb-20">
+        <AliceCarousel
+          mouseTracking
+          items={items}
+          autoPlay
+          disableButtonsControls
+          infinite
+          autoPlayInterval="4000"
+          animationType="fadeout"
         />
-        </div>
-
+      </div>
     </Container>
-  )
+  );
 }
--- src/components/marketing/pageSections/Products.jsx
+++ src/components/marketing/pageSections/Products.jsx
@@ -1,88 +1,94 @@
 import Container from "../../elements/Container";
 import Heading from "../../elements/DisplayTitles/Heading";
 import HeadingTitle from "../../elements/DisplayTitles/HeadingTitle";
-import Button from "../../elements/Button"
-import Product01 from "../../../medias/images/products/Product-1.jpg"
-import Product02 from "../../../medias/images/products/Product-2.jpg"
-import Product03 from "../../../medias/images/products/Product-3.jpg"
+import Button from "../../elements/Button";
+import Product01 from "../../../medias/images/products/Product-1.jpg";
+import Product02 from "../../../medias/images/products/Product-2.jpg";
+import Product03 from "../../../medias/images/products/Product-3.jpg";
 
 export default function Products() {
   return (
     <Container>
+      <HeadingTitle variant="h3">Toujours de délicieux Burgers</HeadingTitle>
 
-        <HeadingTitle variant='h3'>
-            Toujours de délicieux Burgers
-        </HeadingTitle>
+      <Heading theme="secondary" alignement="center" className="my-5">
+        Choisissez et savourez
+      </Heading>
 
-        <Heading theme = "secondary" alignement = "center" className = "my-5">
-            Choisissez et savourez
-        </Heading>
+      <p className="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsum
+        atque labore consequatur perferendis, molestias ipsam quas et accusamus
+        a veritatis quasi iure amet harum, distinctio quaerat aliquam
+        repudiandae fuga!
+      </p>
 
-        <p className="text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsum atque labore consequatur perferendis, molestias ipsam quas et accusamus a veritatis quasi iure amet harum, distinctio quaerat aliquam repudiandae fuga!
-        </p>
+      <div className="grid grid-cols-3 gap-x-1 mb-20 mt-10">
+        <div className="w-full h-full ">
+          <div className="relative w-full h-72">
+            <img
+              src={Product01}
+              alt="image d'un burger avec une viande bien epaise"
+              className="bg-primary absolute top-0 left-0 w-full h-full object-cover z-0"
+            />
+          </div>
 
-        <div className="grid grid-cols-3 gap-x-1 mb-20 mt-10">
-
-            <div className="w-full h-full ">
-                <div className="relative w-full h-72">
-                    <img src={Product01} alt="image d'un burger avec une viande bien epaise" className="bg-primary absolute top-0 left-0 w-full h-full object-cover z-0" />
-                </div>
-
-                <div className="flex flex-col items-center justify-center px-5 pb-5">
-                <Heading variant="h3" className="my-5">
-                    Lorem Ipsum dolor
-                </Heading>
-                <p className="text-center">
-                    Lorem ipsum dolor is simply text for designer and developper.
-                </p>
-                <Button className="font-secondary" color="danger">
-                    Commandez
-                </Button>
-                </div>
-            </div>
-
-            <div className="w-full h-full ">
-                <div className="relative w-full h-72">
-                    <img src={Product02} alt="image d'un burger avec une viande bien epaise" className="bg-primary absolute top-0 left-0 w-full h-full object-cover z-0" />
-                </div>
-
-                <div className="flex flex-col items-center justify-center px-5 pb-5">
-                <Heading variant="h3" className="my-5">
-                    Lorem Ipsum dolor
-                </Heading>
-                <p className="text-center">
-                    Lorem ipsum dolor is simply text for designer and developper.
-                </p>
-                <Button className="font-secondary" color="danger">
-                    Commandez
-                </Button>
-                </div>
-            </div>
-
-            <div className="w-full h-full ">
-                <div className="relative w-full h-72">
-                    <img src={Product03} alt="image d'un burger avec une viande bien epaise" className="bg-primary absolute top-0 left-0 w-full h-full object-cover z-0" />
-                </div>
-
-                <div className="flex flex-col items-center justify-center px-5 pb-5">
-                <Heading variant="h3" className="my-5">
-                    Lorem Ipsum dolor
-                </Heading>
-                <p className="text-center">
-                    Lorem ipsum dolor is simply text for designer and developper.
-                </p>
-                <Button className="font-secondary" color="danger">
-                    Commandez
-                </Button>
-                </div>
-            </div>
-            
-
+          <div className="flex flex-col items-center justify-center px-5 pb-5">
+            <Heading variant="h3" className="my-5">
+              Lorem Ipsum dolor
+            </Heading>
+            <p className="text-center">
+              Lorem ipsum dolor is simply text for designer and developper.
+            </p>
+            <Button className="font-secondary" color="danger">
+              Commandez
+            </Button>
+          </div>
         </div>
 
+        <div className="w-full h-full ">
+          <div className="relative w-full h-72">
+            <img
+              src={Product02}
+              alt="image d'un burger avec une viande bien epaise"
+              className="bg-primary absolute top-0 left-0 w-full h-full object-cover z-0"
+            />
+          </div>
 
+          <div className="flex flex-col items-center justify-center px-5 pb-5">
+            <Heading variant="h3" className="my-5">
+              Lorem Ipsum dolor
+            </Heading>
+            <p className="text-center">
+              Lorem ipsum dolor is simply text for designer and developper.
+            </p>
+            <Button className="font-secondary" color="danger">
+              Commandez
+            </Button>
+          </div>
+        </div>
+
+        <div className="w-full h-full ">
+          <div className="relative w-full h-72">
+            <img
+              src={Product03}
+              alt="image d'un burger avec une viande bien epaise"
+              className="bg-primary absolute top-0 left-0 w-full h-full object-cover z-0"
+            />
+          </div>
+
+          <div className="flex flex-col items-center justify-center px-5 pb-5">
+            <Heading variant="h3" className="my-5">
+              Lorem Ipsum dolor
+            </Heading>
+            <p className="text-center">
+              Lorem ipsum dolor is simply text for designer and developper.
+            </p>
+            <Button className="font-secondary" color="danger">
+              Commandez
+            </Button>
+          </div>
+        </div>
+      </div>
     </Container>
-    
-  )
+  );
 }
