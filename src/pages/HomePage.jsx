acf14adde6c31fec25dcae52eaee8f304e01abcd
AurelienHouse <aurelien.maison@gmail.com>
2024-04-07 00:13:37 +0200

    ✨ Form: Add a form with acerny-component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,16 @@ import Events from "../components/marketing/pageSections/Events";
 import HeroTop from "../components/marketing/pageSections/HeroTop";
 import LastProducts from "../components/marketing/pageSections/LastProducts";
 import Products from "../components/marketing/pageSections/Products";
+import SignupFormDemo from "../components/marketing/pageSections/SignupFormDemo";
 
 export default function HomePage() {
   return (
     <>
-        <HeroTop />
-        <LastProducts />
-        <Products />
-        <Events />
+      <HeroTop />
+      <LastProducts />
+      <Products />
+      <Events />
+      <SignupFormDemo />
     </>
-  )
+  );
 }
